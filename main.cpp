5bdae07b57afc268aa3c01521370f29025c1aae1
Dexyaa <geolundgustuf@gmail.com>
2022-01-29 20:28:39 -0600

    2nd

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <
 using namespace std;
 
 
@@ -7,11 +9,29 @@ int main(){
     int b (0);
     int c{5};
     int result;
+    string mystring;
+    mystring = "hello";
     
     a = a + b;
     result = c - a;
 
-    cout << result;
+    cout << result << endl;
+    cout << mystring << endl;
+
+    cout << "What is your name?" << endl;
+    getline(cin, mystring);
+    cout << "My name is " << mystring << ".\n" << endl;
+    cout << "Type in your favorite number";
+    cin >> a;
+    cout << "Your favorite number is " << a << ".\n";
+
+    if(a == 0){
+        cout << "That's why you have no friends bitch.";
+    else if (a == 1)
+        cout << "You are a winner";
+    else
+        cout << "Fair enough";
+    
 
     return 0;
 }
