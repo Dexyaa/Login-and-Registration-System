d91817cbcdbeaf1f25a91c01ef60e8844891f73f
Dexyaa <geolundgustuf@gmail.com>
2022-01-30 07:58:33 -0600

    3rd

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <
 using namespace std;
 
 
@@ -26,11 +25,29 @@ int main(){
     cout << "Your favorite number is " << a << ".\n";
 
     if(a == 0){
-        cout << "That's why you have no friends bitch.";
-    else if (a == 1)
-        cout << "You are a winner";
-    else
-        cout << "Fair enough";
+        cout << "That's why you have no friends bitch.\n";
+    }
+    else if (a == 1) {
+        cout << "You are a winner. \n";
+    }
+    else {
+        cout << "Fair enough. \n";
+    }
+    int n = 10;
+
+    while (n > 0){
+        cout << n << " : ";
+        n--;
+    }
+
+    string pin;
+
+    do {
+        cout << "Enter the correct pin: " << endl;
+        getline(cin, pin);
+        cout << "The pin you entered: " + pin + " was incorrect." << endl;
+
+    } while (pin != "1024");
     
 
     return 0;
