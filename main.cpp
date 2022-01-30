e2d4acfb561e47e37392c60f966ff1889fe18da8
Dexyaa <geolundgustuf@gmail.com>
2022-01-29 19:46:21 -0600

    1st

--- main.cpp
+++ main.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 
 int main(){
-    cout << "Hello World";
+    int a = 5;
+    int b (0);
+    int c{5};
+    int result;
+    
+    a = a + b;
+    result = c - a;
+
+    cout << result;
+
+    return 0;
 }
