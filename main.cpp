26b280e15f54678923f52240b856e2ec4cbc7def
Geodude <87618311+Dexyaa@users.noreply.github.com>
2022-03-17 16:58:35 +0000

    I changed everything

--- main.cpp
+++ main.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <fstream>
 #include <filesystem>
 using namespace std;
 
-namespace fs = std::filesystem;
 
-//
+int c;
+void login();
+void registers();
 
 
 int main(){
-  vector<string> filesToCheck = {"database"};
+  cout << "Welcome to the login page" << endl;
+  cout << "What would you like to do?" << endl;
+  cout << "1) Login" << endl;
+  cout << "2) Register" << endl;
+  cout << "3) Leave" << endl;
 
-  for(const auto &file : filesToCheck){
-    if(!exists(file)){
-      cout << "Doesn't exist\n";
-      ofstream Database("database.txt");
-    } else {
-      cout << "The file exists" << endl;
-    }
+
+  cin >> c;
+
+  switch(c){
+    case 1:
+      login();
+      break;
+    case 2:
+      registers();
+      break;
+    case 3:
+      return EXIT_SUCCESS;
+      break;
+    default:
+      cout << "Select a number between 1 to 3";
+      main();
   }
-  
+
+  void login(){
+    string username, password;
+    cout << "Enter your username and password" << endl;
+    cin >> username;
+    cin >> password;
+
+    cout << "hi" << endl;
+  }
+
+  void registers(){
+    string username, password;
+    cout << "hi" << endl;
+  }
 }
