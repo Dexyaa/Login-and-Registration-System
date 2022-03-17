f7aafb8e11f806ab3df394157bf0b7c0a6fc681d
Geodude <87618311+Dexyaa@users.noreply.github.com>
2022-03-17 13:23:38 +0000

    i added stuff

--- main.cpp
+++ main.cpp
@@ -1,54 +1,25 @@
 #include <iostream>
+#include <vector>
 #include <string>
+#include <fstream>
+#include <filesystem>
 using namespace std;
+
+namespace fs = std::filesystem;
+
+//
 
 
 int main(){
-    int a = 5;
-    int b (0);
-    int c{5};
-    int result;
-    string mystring;
-    mystring = "hello";
-    
-    a = a + b;
-    result = c - a;
+  vector<string> filesToCheck = {"database"};
 
-    cout << result << endl;
-    cout << mystring << endl;
-
-    cout << "What is your name?" << endl;
-    getline(cin, mystring);
-    cout << "My name is " << mystring << ".\n" << endl;
-    cout << "Type in your favorite number";
-    cin >> a;
-    cout << "Your favorite number is " << a << ".\n";
-
-    if(a == 0){
-        cout << "That's why you have no friends bitch.\n";
+  for(const auto &file : filesToCheck){
+    if(!exists(file)){
+      cout << "Doesn't exist\n";
+      ofstream Database("database.txt");
+    } else {
+      cout << "The file exists" << endl;
     }
-    else if (a == 1) {
-        cout << "You are a winner. \n";
-    }
-    else {
-        cout << "Fair enough. \n";
-    }
-    int n = 10;
-
-    while (n > 0){
-        cout << n << " : ";
-        n--;
-    }
-
-    string pin;
-
-    do {
-        cout << "Enter the correct pin: " << endl;
-        getline(cin, pin);
-        cout << "The pin you entered: " + pin + " was incorrect." << endl;
-
-    } while (pin != "1024");
-    
-
-    return 0;
+  }
+  
 }
